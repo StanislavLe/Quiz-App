5d1665e89097ed7646cbf371ae2fef186f1d6c9c
Stanislav Levin <stanislav2708@outlook.de>
2024-05-31 17:27:05 +0200

    JSON complete

--- script.js
+++ script.js
@@ -27,37 +27,37 @@ let questions = [
     },
     {
         "frage": "Was ist ein Terminal?",
-        "antwort1": "JustinBiber",
-        "antwort2": "AndrewTate",
-        "antwort3": "Tim Berners-Lee",
-        "antwort4": "ThomasLee",
+        "antwort1": "ausführung von befehlen",
+        "antwort2": "die eingabe von Befehlen ",
+        "antwort3": "das Feld zum eingeben von Befehlen",
+        "antwort4": "Toastbrot",
         "rightAnswer": 3
     },
 
     {
-        "frage": "Wer hat Html erfunden?",
-        "antwort1": "JustinBiber",
-        "antwort2": "AndrewTate",
+        "frage": "was bedeutet display:Flex?",
+        "antwort1": "anordnung von Elementen",
+        "antwort2": "neue Konsole",
         "antwort3": "Tim Berners-Lee",
         "antwort4": "ThomasLee",
-        "rightAnswer": 3
+        "rightAnswer": 1
     },
 
     {
-        "frage": "Wer hat Html erfunden?",
+        "frage": "Wer hip hop geprägt?",
         "antwort1": "JustinBiber",
         "antwort2": "AndrewTate",
         "antwort3": "Tim Berners-Lee",
-        "antwort4": "ThomasLee",
-        "rightAnswer": 3
+        "antwort4": "2Pac",
+        "rightAnswer": 4
     },
 
     {
-        "frage": "Wer hat Html erfunden?",
+        "frage": "Wo ist die information zu den templates zu finden?",
         "antwort1": "JustinBiber",
-        "antwort2": "AndrewTate",
-        "antwort3": "Tim Berners-Lee",
-        "antwort4": "ThomasLee",
-        "rightAnswer": 3
+        "antwort2": "html",
+        "antwort3": "css",
+        "antwort4": "script",
+        "rightAnswer": 4
     }
 ]
