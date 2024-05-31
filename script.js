319c6a971ab84d699b232508ff23bfdd6a5d4168
Stanislav Levin <stanislav2708@outlook.de>
2024-05-31 17:21:15 +0200

    addJSONArray

---
+++ script.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        "frage": "Wer hat Html erfunden?",
+        "antwort1": "JustinBiber",
+        "antwort2": "AndrewTate",
+        "antwort3": "Tim Berners-Lee",
+        "antwort4": "ThomasLee",
+        "rightAnswer": 3
+    },
+
+    {
+        "frage": "Woher kommt das i in der for Schleife?",
+        "antwort1": "index",
+        "antwort2": "inshaAllah",
+        "antwort3": "interlaken",
+        "antwort4": "insburg",
+        "rightAnswer": 1
+    },
+
+    {
+        "frage": "wo kann man seine projekte hochladen?",
+        "antwort1": "iCloud",
+        "antwort2": "Github",
+        "antwort3": "Facebook",
+        "antwort4": "Tinder",
+        "rightAnswer": 2
+    },
+    {
+        "frage": "Was ist ein Terminal?",
+        "antwort1": "JustinBiber",
+        "antwort2": "AndrewTate",
+        "antwort3": "Tim Berners-Lee",
+        "antwort4": "ThomasLee",
+        "rightAnswer": 3
+    },
+
+    {
+        "frage": "Wer hat Html erfunden?",
+        "antwort1": "JustinBiber",
+        "antwort2": "AndrewTate",
+        "antwort3": "Tim Berners-Lee",
+        "antwort4": "ThomasLee",
+        "rightAnswer": 3
+    },
+
+    {
+        "frage": "Wer hat Html erfunden?",
+        "antwort1": "JustinBiber",
+        "antwort2": "AndrewTate",
+        "antwort3": "Tim Berners-Lee",
+        "antwort4": "ThomasLee",
+        "rightAnswer": 3
+    },
+
+    {
+        "frage": "Wer hat Html erfunden?",
+        "antwort1": "JustinBiber",
+        "antwort2": "AndrewTate",
+        "antwort3": "Tim Berners-Lee",
+        "antwort4": "ThomasLee",
+        "rightAnswer": 3
+    }
+]
