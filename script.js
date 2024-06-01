308bc9a473a2aa6333b8a08c7a38ae2bb2035849
Stanislav Levin <stanislav2708@outlook.de>
2024-06-01 11:22:46 +0200

    complete card + JSONArray

--- script.js
+++ script.js
@@ -1,90 +1,90 @@
 let questions = [
     {
         "frage": " Welcher pH-Wert des Wassers ist optimal für den Anbau von Cannabis in Erde?",
-        "antwort1": "5.0",
-        "antwort2": "6.5",
-        "antwort3": "7.5",
-        "antwort4": "8.0",
+        "answer1": "5.0",
+        "answer2": "6.5",
+        "answer3": "7.5",
+        "answer4": "8.0",
         "rightAnswer": 2
     },
 
     {
         "frage": " Was kann passieren, wenn der pH-Wert des Wassers zu niedrig ist?",
-        "antwort1": "Verbessertes Nährstoffaufnahme",
-        "antwort2": "Blockade von Nährstoffen",
-        "antwort3": "Erhöhte Wachstumsgeschwindigkeit",
-        "antwort4": "Keine Auswirkungen",
+        "answer1": "Verbessertes Nährstoffaufnahme",
+        "answer2": "Blockade von Nährstoffen",
+        "answer3": "Erhöhte Wachstumsgeschwindigkeit",
+        "answer4": "Keine Auswirkungen",
         "rightAnswer": 2
     },
 
     {
         "frage": "Was beschreibt der VPD-Wert im Cannabisanbau?",
-        "antwort1": "Verhältnis von Lichtintensität zu Pflanzenhöhe",
-        "antwort2": "Differenz zwischen Luftfeuchtigkeit und Sättigungsfeuchtigkeit",
-        "antwort3": "pH-Wert des Bodens",
-        "antwort4": "Menge des zugeführten CO2",
+        "answer1": "Verhältnis von Lichtintensität zu Pflanzenhöhe",
+        "answer2": "Differenz zwischen Luftfeuchtigkeit und Sättigungsfeuchtigkeit",
+        "answer3": "pH-Wert des Bodens",
+        "answer4": "Menge des zugeführten CO2",
         "rightAnswer": 2
     },
     {
         "frage": "Welcher VPD-Wert wird für die Blütephase von Cannabis empfohlen?",
-        "antwort1": "0.2 kPa",
-        "antwort2": "0.8 kPa",
-        "antwort3": "1.2 kPa",
-        "antwort4": "2.5 kPa",
+        "answer1": "0.2 kPa",
+        "answer2": "0.8 kPa",
+        "answer3": "1.2 kPa",
+        "answer4": "2.5 kPa",
         "rightAnswer": 3
     },
 
     {
         "frage": "Welches Symptom ist typisch für Stickstoffmangel bei Cannabispflanzen?",
-        "antwort1": "Gelbe Blätter an den unteren Teilen der Pflanze",
-        "antwort2": "Braun verfärbte Blattspitzen",
-        "antwort3": "Weiße Flecken auf den Blättern",
-        "antwort4": "Verwelkte Blüten",
+        "answer1": "Gelbe Blätter an den unteren Teilen der Pflanze",
+        "answer2": "Braun verfärbte Blattspitzen",
+        "answer3": "Weiße Flecken auf den Blättern",
+        "answer4": "Verwelkte Blüten",
         "rightAnswer": 1
     },
 
     {
         "frage": "Was könnte ein Zeichen für einen Überschuss an Kalium sein?",
-        "antwort1": "Dunkelgrüne, glänzende Blätter",
-        "antwort2": "Eingekrümmte Blattspitzen und Ränder",
-        "antwort3": "Zähe, verdickte Stängel",
-        "antwort4": "Langsames Wachstum",
+        "answer1": "Dunkelgrüne, glänzende Blätter",
+        "answer2": "Eingekrümmte Blattspitzen und Ränder",
+        "answer3": "Zähe, verdickte Stängel",
+        "answer4": "Langsames Wachstum",
         "rightAnswer": 2
     },
 
     {
         "frage": "Welche Lichtfarbe fördert hauptsächlich das vegetative Wachstum von Cannabispflanzen?",
-        "antwort1": "Rot",
-        "antwort2": "Blau",
-        "antwort3": "Grün",
-        "antwort4": "Gelb",
+        "answer1": "Rot",
+        "answer2": "Blau",
+        "answer3": "Grün",
+        "answer4": "Gelb",
         "rightAnswer": 2
     },
 
     {
         "frage": "Welches Medium wird oft verwendet, um die Wurzeln von Cannabispflanzen zu belüften?",
-        "antwort1": "Perlit",
-        "antwort2": "Sand",
-        "antwort3": "Ton",
-        "antwort4": "Lehm",
+        "answer1": "Perlit",
+        "answer2": "Sand",
+        "answer3": "Ton",
+        "answer4": "Lehm",
         "rightAnswer": 1
     },
 
     {
         "frage": "Welche Technik wird angewendet, um Cannabispflanzen in kleinere, handlichere Formen zu zwingen?",
-        "antwort1": "Topping",
-        "antwort2": "Flushing",
-        "antwort3": "Cloning",
-        "antwort4": "Lollipopping",
+        "answer1": "Topping",
+        "answer2": "Flushing",
+        "answer3": "Cloning",
+        "answer4": "Lollipopping",
         "rightAnswer": 1
     },
 
     {
         "frage": "Was ist ein häufiges Anzeichen für Wurzelfäule?",
-        "antwort1": "Weiße, gesunde Wurzeln",
-        "antwort2": "Braun verfärbte und schleimige Wurzeln",
-        "antwort3": "Schnell wachsendes Wurzelsystem",
-        "antwort4": "Vermehrtes Blattwachstum",
-        "rightAnswer": 2
+        "answer1": "Weiße, gesunde Wurzeln",
+        "answer2": "Braun verfärbte und schleimige Wurzeln",
+        "answer3": "Schnell wachsendes Wurzelsystem",
+        "answer4": "Vermehrtes Blattwachstum",
+        "rightAnswer":1
     }
 ]
