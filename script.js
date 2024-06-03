4374c00f7d4e87702e4fddf47525d4e0463d1d2d
Stanislav Levin <stanislav2708@outlook.de>
2024-06-03 03:53:35 +0200

    process till endscreen

--- script.js
+++ script.js
@@ -89,37 +89,77 @@ let questions = [
     }
 ]
 
+let rightQuestions = 0;
+
 let currentQuestion = 0;
+
 
 function render() {
     document.getElementById('questionListLength').innerHTML = questions.length;
     showQuestion();
 }
 
+
 function showQuestion() {
-    let question = questions[currentQuestion];
-    document.getElementById('questionField').innerHTML = question['question'];
-    document.getElementById('answer1').innerHTML = question['answer1'];
-    document.getElementById('answer2').innerHTML = question['answer2'];
-    document.getElementById('answer3').innerHTML = question['answer3'];
-    document.getElementById('answer4').innerHTML = question['answer4'];
+    if (currentQuestion >= questions.length) {
+        showEndscreen();
+    } else {
+        let question = questions[currentQuestion];
+        document.getElementById('questionNumber').innerHTML = currentQuestion + 1;
+        document.getElementById('questionField').innerHTML = question['question'];
+        document.getElementById('answer1').innerHTML = question['answer1'];
+        document.getElementById('answer2').innerHTML = question['answer2'];
+        document.getElementById('answer3').innerHTML = question['answer3'];
+        document.getElementById('answer4').innerHTML = question['answer4'];
+    }
 }
+
+
+function showEndscreen() {
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none;'
+    document.getElementById('questionField').style = 'display: none;'
+    document.getElementById('cardTitel').style = 'display: none;'
+    document.getElementById('quizBackground').style = 'display: none;'
+    document.getElementById('questionListLength2').innerHTML = questions.length;
+    document.getElementById('rightAnswers').innerHTML = rightQuestions;
+}
+
+
+
 
 function answer(selection) {
     let question = questions[currentQuestion];
-    console.log('selected answer is', selection);
     let selectedQuestionNumber = selection.slice(-1);
-    console.log('selected Questionnumber is', selectedQuestionNumber)
-    console.log('right Answer is', question['rightAnswer']);
-
     let idOfRightAnswer = `answer${question['rightAnswer']}`;
-
     if (selectedQuestionNumber == question['rightAnswer']) {
-        console.log('richtige Antwort');
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        rightQuestions ++;
     } else {
-        console.log('falsche Antwort')
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
+    document.getElementById('next-button').disabled = false;
 }
+
+
+function nextQuestion() {
+    currentQuestion++;
+    resetAnswerButtons();
+    showQuestion();
+}
+
+
+function resetAnswerButtons() {
+    document.getElementById('next-button').disabled = true;
+    document.getElementById('answer1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer4').parentNode.classList.remove('bg-success');
+}
+
+
